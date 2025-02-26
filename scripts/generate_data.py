71430fcb42156959cb81a691a07538d6327fc161
Hugo Buurmeijer <hugo.buurmeijer@gmail.com>
2025-02-26 15:03:28 -0800

    Add data saving args

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,29 +1,33 @@
-import argparse
 import os
+import argparse
+from tqdm import tqdm
 
 os.environ["MUJOCO_GL"] = "egl"  # Avoids issues with GLFWError on Linux TODO: Patrick
 
 from trunk_sim.simulator import TrunkSimulator, get_model_path
+from trunk_sim.data import TrunkData
+from trunk_sim.policy import TrunkPolicy
 from trunk_sim.rollout import rollout
-from trunk_sim import data
 
 
 def main(args):
     simulator = TrunkSimulator(get_model_path(args.model_type))
-    # data = ...
-    # policy = ...
+    data = TrunkData(states="pos_vel", links=[1, 2, 3])
+    policy = TrunkPolicy(lambda _: simulator.get_random_control())
 
-    rollout(
-        simulator,
-        num_rollouts=args.num_rollouts,
-        duration_s=args.duration_s,
-        timestep_ms=args.timestep_ms,
-        render_video=args.render_video,
-        video_filename=args.video_filename,
-    )
+    for rollout_idx in tqdm(range(1, args.num_rollouts + 1)):
+        initial_state = simulator.get_random_state()
+        rollout(
+            data,
+            simulator,
+            policy,
+            initial_state=initial_state,
+            duration=args.duration,
+            render_video=args.render_video,
+            video_filename=args.videos_dir + f"rollout_{rollout_idx}.mp4",
+        )
 
-    # Save data
-    # data.save("data.pkl")
+    data.save_to_csv(args.data_filename)
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -34,33 +38,26 @@ def parse_args():
         "--num_rollouts", type=int, default=1, help="Number of rollouts to perform."
     )
     parser.add_argument(
-        "--duration_s",
+        "--duration",
         type=float,
         default=1.0,
-        help="Duration of the rollout in seconds.",
-    )
-    parser.add_argument(
-        "--timestep_ms",
-        type=float,
-        default=10,
-        help="Timestep of the rollout in milliseconds.",
+        help="Duration of each rollout in seconds.",
     )
     parser.add_argument(
         "--render_video", action="store_true", help="Render video of the rollout."
     )
     parser.add_argument(
-        "--video_filename",
+        "--video_dir",
         type=str,
-        default="render.mpy",
-        help="Filename of the rendered video.",
+        default="trunk_videos/",
+        help="Directory of the rendered video.",
     )
-    #TODO: Hugo add data_filename and policy
-    # parser.add_argument(
-    #     "--data_filename",
-    #     type=str,
-    #     default="data.csv",
-    #     help="Filename to save the data.",
-    # )
+    parser.add_argument(
+        "--data_filename",
+        type=str,
+        default="data.csv",
+        help="Filename to save the data.",
+    )
     return parser.parse_args()
 
 if __name__ == "__main__":
