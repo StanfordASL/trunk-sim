19d1956ec6199b5a8f41e40f1c2b9db2c181e852
Patrick Benito <peberhard@ethz.ch>
2025-02-26 18:47:02 -0800

    corrected rollout for None policy

--- src/trunk_sim/rollout.py
+++ src/trunk_sim/rollout.py
@@ -8,47 +8,51 @@ from trunk_sim.policy import TrunkPolicy
 from trunk_sim.data import TrunkData
 
 
-def rollout(data: TrunkData,
-            simulator: TrunkSimulator,
-            policy: TrunkPolicy = None,
+def rollout(simulator: TrunkSimulator,
+            policy: Optional[TrunkPolicy] = None,
+            data: Optional[TrunkData] = None,
             initial_state: Optional[Tuple[np.ndarray, np.ndarray]] = None,
             duration: float = 1.0,  # [s]
             render_video: bool = False,
             framerate: int = 30,  # [Hz]
-            video_filename: Optional[str] = "trunk_render.mpy"):
+            video_filename: Optional[str] = "trunk_render.mpy",
+            stop_at_convergence: bool = False) -> None:
     """
     Rollout a policy on a simulator and save it inside a data object.
     """
     simulator.reset()
-    if initial_state is not None:
-        simulator.set_state(*initial_state)
-    state = simulator.get_state()
+
     converged = False
     
     if render_video:
         frames = []
 
         with mujoco.Renderer(simulator.model) as renderer:
-            while simulator.data.time < duration and not converged:
-                control_input = policy(state)
-                t, state_new, converged = simulator.step(control_input)
-                data.add_data(t, state, control_input)
+            while simulator.data.time < duration and (not stop_at_convergence or not converged):
+                rollout_step(simulator, policy, data)
                 
                 # Rendering
                 renderer.update_scene(simulator.data)
                 pixels = renderer.render()
                 frames.append(pixels)
 
-                # Update state
-                state = state_new
-
         media.write_video(video_filename, frames, fps=framerate)
         
     else:
-        while simulator.data.time < duration and not converged:
-            control_input = policy(state)
-            t, state_new, converged = simulator.step(control_input)
-            data.add_data(t, state, control_input)
+        while simulator.data.time < duration and (not stop_at_convergence or not converged):
+            rollout_step(simulator, policy, data)
 
-            # Update state
-            state = state_new
+
+def rollout_step(simulator: TrunkSimulator,
+                 policy: Optional[TrunkPolicy] = None,
+                 data: Optional[TrunkData] = None) -> None:
+    """
+    Perform a single step of a rollout.
+    """
+    state = simulator.get_state()
+    control_input = policy(state) if policy is not None else None
+        
+    t, _, converged = simulator.step(control_input)
+
+    if data is not None:
+        data.add_data(t, state, control_input)
